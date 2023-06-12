554c3f1f5527f9d65483d956bfbda80f26d5083b
Rodrigoa2197 <00303318@uca.edu.sv>
2023-06-12 17:18:30 -0600

    modified: carpeta codigo

---
+++ Codigos/practica11.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+/*
+Cuanto aumentara el costo del parque después de 30 min: $0.50
+Aquí pídanle al usuario cuanto tiempo paso parqueado 
+Cin>> min
+For(int i = 0; i<min; i++){}
+*/
+void slogan(){
+	
+}
+
+int main(){
+	int opcionPrincipal;
+	cout <<"Menu Principal"
+	cout <<"1) Generar el slogan";
+	cout <<"2) Opciones usuario administrador";
+	cout <<"3) Opciones usuario general";
+	cin >> opcionPrincipal
+	switch(opcionPrincipal){
+		case 1: slogan(); break;
+		case 2: 
+			cout <<"Opciones usuario administrador";
+			cout <<"Mostrar la cantidad por de tipo de vehículos parqueados";
+			cout <<"Mostrar los autos, motos de mayor ingreso al parqueo";
+			cout <<"Mostrar la cantidad de parqueos disponibles y no disponibles";
+			cout <<"Bloquear parqueos específicos (capacidades especiales, embarazadas)";
+							
+		Bloquear parqueos específicos (capacidades especiales, embarazadas).
+		Generar  un registro  de  tickets de parqueo  a través de la creación de un archivo  txt  por cada día de la semana (lunes,martes,miércoles,viernes).
+
+		break;
+		case 3: break;
+	}
+	return 0;
+}
